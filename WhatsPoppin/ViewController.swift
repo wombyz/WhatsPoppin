a41fb861d71d4eadfdcd91ee9147ed388ffda393
Liam Ottley <liamottley@dykemodge.local>
2018-05-29 18:54:18 +1200

    Initial Commit

---
+++ WhatsPoppin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WhatsPoppin
+//
+//  Created by Liam Ottley on 29/05/18.
+//  Copyright © 2018 Liam Ottley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
